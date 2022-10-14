b968d0d38e7e7382af6fbad7fa3fb5f4964dd04f
MXNXV-ERR <MXNXV-ERR@users.noreply.github.com>
2022-10-14 22:00:32 +0530

    prog3.py Updated

--- prog3.py
+++ prog3.py
@@ -14,11 +14,11 @@ def ftok(a):
     return 273.5 + ftoc(a)
 def ktof(a):
     return 1.8*(ctok(a)) + 32
-
+#infinite loop for menu driven approach
 while(1):
     print("Main Menu".center(40,"#"))
     ch=int(input("1.Conversion\n2.History\n3.EXIT\nEnter your choice(int only):"))
-    if ch==1:
+    if ch==1:#conversion based on user choice
         n=input("Enter temperature you would like to convert with unit(Eg:20C,50.00k...):")
         val=float(n[:-1])
         unit=n[-1].upper()
@@ -41,15 +41,16 @@ while(1):
         else:
             print("Invalid input!!!")
             continue
+        #save the conversion in tuple and append to list
         print("Ans:"+str(ans)+str(to))
         x=[val,unit,"to",round(ans,3),to]
         history.append(tuple(x))
-    elif ch==2:
+    elif ch==2:#print all the executed conversions
         c=input("1.Order by from-value\n2.Order by to-value\nEnter your choice:")
         print("😏=H=I=S=T=O=R=Y".center(40,"="))
-        if c=='1':
+        if c=='1':#ordered by from-value
             print(*sorted(history,key=lambda x:x[0]),sep="\n")
-        elif c=='2':
+        elif c=='2':#ordered by to-value
             print(*sorted(history,key=lambda x:x[3]),sep="\n")
         else:
             print("Invalid input,kid!!!")
