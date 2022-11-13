6354dd557830700187efd5adea87e6b7cde3b2d6
MXNXV-ERR <MXNXV-ERR@users.noreply.github.com>
2022-11-13 12:37:29 +0530

    partb q1 it seems

---
+++ B_Question1.py
@@ -0,0 +1,24 @@
+# Python Classes: Write a python class to reverse a sentence (initialized via constructor) word
+# by word. Example: “I am here” should be reversed as “here am I”. Create instances of this
+# class for each of the three strings input by the user and display the reversed string for each, in
+# descending order of number of vowels in the string.
+
+class Reverse:
+    string=""
+    def __init__(self,string):
+        self.string=" ".join(reversed(string.split()))
+       
+
+print("Enter 3 strings")
+objArr=[]
+countVowels = [0, 0, 0]
+dict = {}
+for i in range(3):
+    objArr.append(Reverse(input()))
+    for j in range(len(objArr[i].string)):
+        if objArr[i].string[j].lower() in ['a', 'e', 'i', 'o', 'u']:
+             countVowels[i] += 1
+
+    dict.update({objArr[i].string:countVowels[i]})
+print("Sorted in descending order of vowels in each")
+print(*sorted(dict.items(),key=lambda x:x[1],reverse=True),sep="\n")
